e89e40dad1499f9dc13b9e55d3a4add1975ca631
Alen Ajam <hello@alenajam.dev>
2025-02-28 20:07:14 +0100

    chore: add asset and debt types constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,31 @@
 export const APP_CURRENCY = "EUR";
+
+export const ASSET_TYPES = [
+  "Cash",
+  "Bank Accounts",
+  "Stocks",
+  "Bonds",
+  "Retirement Accounts",
+  "Real Estate",
+  "Vehicles",
+  "Collectibles",
+  "Business Interests",
+  "Intellectual Property",
+  "Other",
+] as const;
+
+export const DEBT_TYPES = [
+  "Mortgage",
+  "Home Equity Loans/HELOC",
+  "Car Loans",
+  "Auto Leases",
+  "Student Loans",
+  "Credit Cards",
+  "Personal Loans",
+  "Retail Financing",
+  "Business Loans",
+  "Medical Debt",
+  "Tax Liabilities",
+  "Short-term Debt",
+  "Other",
+] as const;
