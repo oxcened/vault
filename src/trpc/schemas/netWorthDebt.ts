bf43ba2861e347bcd941bef3b63b1a78f7a02d46
Alen Ajam <hello@alenajam.dev>
2025-03-01 11:46:01 +0100

    refactor(nw-debt): add labels to create schema

--- src/trpc/schemas/netWorthDebt.ts
+++ src/trpc/schemas/netWorthDebt.ts
@@ -3,21 +3,28 @@ import * as yup from "yup";
 import { OTHER_TYPE } from "~/constants";
 
 export const createNetWorthDebtSchema = yup.object({
-  name: yup.string().required(),
-  type: yup.string().required(),
-  customType: yup.string().when("type", {
-    is: OTHER_TYPE,
-    then: (schema) => schema.required(),
-  }),
-  currency: yup.string().required(),
-  initialQuantity: yup.number().required(),
-  quantityFormula: yup.string().test((value) => {
-    if (value === undefined) return true;
-    try {
-      evaluate(value);
-      return true;
-    } catch (e) {
-      return false;
-    }
-  }),
+  name: yup.string().label("Name").required(),
+  type: yup.string().label("Type").required(),
+  customType: yup
+    .string()
+    .label("Custom type")
+    .when("type", {
+      is: OTHER_TYPE,
+      then: (schema) => schema.required(),
+    }),
+  currency: yup.string().label("Currency").required(),
+  initialQuantity: yup.number().label("Initial quantity").required(),
+  quantityFormula: yup
+    .string()
+    .label("Quantity formula")
+    .test((value) => {
+      if (!value) return true;
+
+      try {
+        evaluate(value);
+        return true;
+      } catch (e) {
+        return false;
+      }
+    }),
 });
