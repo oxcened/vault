d3d4dcb2f9c843866d0567796dc159581faf566a
Alen Ajam <hello@alenajam.dev>
2025-03-08 17:05:08 +0100

    chore: removed unused imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import Link from "next/link";
 import { redirect } from "next/navigation";
 import { buttonVariants } from "~/components/ui/button";
-import { Card, CardContent, CardHeader } from "~/components/ui/card";
 import { auth } from "~/server/auth";
 import { HydrateClient } from "~/trpc/server";
 
