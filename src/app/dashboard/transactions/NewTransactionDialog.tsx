bae3d028a5bed12c5c89bc1e4884e56d127f05dc
Alen Ajam <alen@fleek.xyz>
2025-03-08 08:55:17 +0100

    feat(transaction): add new transaction dialog

---
+++ src/app/dashboard/transactions/NewTransactionDialog.tsx
@@ -0,0 +1,202 @@
+"use client";
+
+import { Loader2 } from "lucide-react";
+import { useState, useEffect } from "react";
+import { Button } from "~/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "~/components/ui/dialog";
+import { Input } from "~/components/ui/input";
+import { api } from "~/trpc/react";
+import { useForm } from "react-hook-form";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "~/components/ui/select";
+import { APP_CURRENCY } from "~/constants";
+import { Switch } from "~/components/ui/switch";
+import { Label } from "~/components/ui/label";
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "~/components/ui/form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { toast } from "sonner";
+import { createTransactionSchema } from "~/trpc/schemas/transaction";
+import { TransactionType } from "@prisma/client";
+
+export type NewTransactionDialogProps = {
+  isOpen: boolean;
+  onOpenChange: (newOpen: boolean) => void;
+  onSuccess: () => void;
+};
+
+export default function NewTransactionDialog({
+  isOpen,
+  onOpenChange,
+  onSuccess,
+}: NewTransactionDialogProps) {
+  const [createMore, setCreateMore] = useState(false);
+  const { mutate, isPending } = api.transaction.create.useMutation({
+    onSuccess: () => {
+      toast.success("Transaction created.");
+      if (createMore) {
+        form.reset();
+      } else {
+        onOpenChange(false);
+      }
+      onSuccess();
+    },
+  });
+  const form = useForm({
+    defaultValues: {
+      currency: APP_CURRENCY,
+      categoryId: "",
+      description: "",
+      timestamp: new Date(),
+      type: "" as TransactionType,
+    },
+    resolver: yupResolver(createTransactionSchema),
+  });
+
+  useEffect(() => {
+    if (!isOpen) return;
+    form.reset();
+  }, [isOpen]);
+
+  return (
+    <Dialog open={isOpen} onOpenChange={onOpenChange}>
+      <DialogContent>
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit((data) => mutate(data))}
+            className="flex flex-col gap-5"
+          >
+            <DialogHeader>
+              <DialogTitle>New transaction</DialogTitle>
+            </DialogHeader>
+            <div className="flex flex-col gap-2">
+              <FormField
+                control={form.control}
+                name="type"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Type</FormLabel>
+
+                    <Select value={field.value} onValueChange={field.onChange}>
+                      <FormControl>
+                        <SelectTrigger>
+                          <SelectValue placeholder="Select a type" />
+                        </SelectTrigger>
+                      </FormControl>
+                      <SelectContent>
+                        {Object.values(TransactionType).map((type) => (
+                          <SelectItem key={type} value={type}>
+                            {type}
+                          </SelectItem>
+                        ))}
+                      </SelectContent>
+                    </Select>
+
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={form.control}
+                name="description"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Description</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Description" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={form.control}
+                name="amount"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Amount</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="Amount"
+                        type="number"
+                        step="any"
+                        {...field}
+                        value={field.value ?? ""}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={form.control}
+                name="currency"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Currency</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="Currency (e.g., USD, EUR, BTC)"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={form.control}
+                name="categoryId"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Category ID</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Category ID" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+
+            <DialogFooter className="gap-2">
+              <div className="flex items-center space-x-2">
+                <Switch
+                  id="airplane-mode"
+                  onCheckedChange={(checked) => setCreateMore(checked)}
+                />
+                <Label htmlFor="airplane-mode">Create more</Label>
+              </div>
+              <Button type="submit" disabled={isPending}>
+                {isPending && <Loader2 className="animate-spin" />}
+                Save
+              </Button>
+            </DialogFooter>
+          </form>
+        </Form>
+      </DialogContent>
+    </Dialog>
+  );
+}
