009f339f46ece55195ba95f20cd3f1234f318c39
Alen Ajam <hello@alenajam.dev>
2025-03-08 19:51:09 +0100

    chore(transaction): page responsiveness

--- src/app/dashboard/transactions/page.tsx
+++ src/app/dashboard/transactions/page.tsx
@@ -30,18 +30,16 @@ import {
   DropdownMenuTrigger,
 } from "~/components/ui/dropdown-menu";
 import { MoreHorizontal, Plus } from "lucide-react";
-import { ExchangeRate } from "@prisma/client";
 import { TableSkeleton } from "~/components/table-skeleton";
 import { toast } from "sonner";
-import { Currency, Number } from "~/components/ui/number";
+import { Currency } from "~/components/ui/number";
 import { cn } from "~/lib/utils";
-import NewAssetDialog from "../assets/NewAssetDialog";
 import NewTransactionDialog from "./NewTransactionDialog";
+import { useBreakpoint } from "~/hooks/useBreakpoint";
 
 export default function TransactionsPage() {
   const { data = [], refetch, isPending } = api.transaction.getAll.useQuery();
 
-  // Delete mutation.
   const { mutate: deleteTransaction } = api.transaction.delete.useMutation({
     onSuccess: () => {
       toast.success("Transaction deleted.");
@@ -50,6 +48,8 @@ export default function TransactionsPage() {
   });
 
   const [isNewTransaction, setNewTransaction] = useState(false);
+
+  const { md } = useBreakpoint();
 
   return (
     <>
@@ -60,7 +60,7 @@ export default function TransactionsPage() {
           <BreadcrumbList>
             <BreadcrumbItem>
               <BreadcrumbLink href="/dashboard/net-worth">
-                Net Worth
+                Cash flow
               </BreadcrumbLink>
             </BreadcrumbItem>
             <BreadcrumbSeparator />
@@ -73,14 +73,15 @@ export default function TransactionsPage() {
         <Button
           variant="outline"
           className="ml-auto"
+          size={md ? "default" : "icon"}
           onClick={() => setNewTransaction(true)}
         >
           <Plus />
-          New transaction
+          <span className="hidden md:inline">New transaction</span>
         </Button>
       </header>
 
-      <div className="mx-auto w-full max-w-screen-md p-5">
+      <div className="mx-auto w-screen max-w-screen-md p-5">
         {isPending && <TableSkeleton />}
         {!isPending && !data.length && (
           <div className="rounded-xl bg-muted p-10 text-center text-muted-foreground">
@@ -88,66 +89,83 @@ export default function TransactionsPage() {
           </div>
         )}
         {!isPending && !!data.length && (
-          <Table>
-            <TableHeader>
-              <TableRow>
-                <TableHead>Date</TableHead>
-                <TableHead>Description</TableHead>
-                <TableHead>Category</TableHead>
-                <TableHead className="text-right">Amount</TableHead>
-                <TableHead></TableHead>
-              </TableRow>
-            </TableHeader>
-            <TableBody>
-              {data.map((row) => {
-                const isIncome = row.type === "INCOME";
-                const isExpense = row.type === "EXPENSE";
-                const amount = row.amount.mul(isExpense ? -1 : 1);
+          <div>
+            <Table className="whitespace-nowrap">
+              <TableHeader>
+                <TableRow>
+                  <TableHead>Date</TableHead>
+                  <TableHead>Description</TableHead>
+                  <TableHead className="hidden md:table-cell">
+                    Category
+                  </TableHead>
+                  <TableHead className="text-right">Amount</TableHead>
+                  <TableHead className="w-8"></TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                {data.map((row) => {
+                  const isIncome = row.type === "INCOME";
+                  const isExpense = row.type === "EXPENSE";
+                  const amount = row.amount.mul(isExpense ? -1 : 1);
 
-                return (
-                  <TableRow key={row.id}>
-                    <TableCell>
-                      {formatDate({
-                        date: row.timestamp,
-                      })}
-                    </TableCell>
-                    <TableCell>{row.description}</TableCell>
-                    <TableCell>{row.category.name}</TableCell>
-                    <TableCell className="text-right">
-                      <Currency
-                        value={amount}
-                        options={{
-                          currency: row.currency,
-                        }}
-                        className={cn(
-                          isIncome && "text-green-600",
-                          isExpense && "text-red-600",
-                        )}
-                      />
-                    </TableCell>
-                    <TableCell>
-                      <DropdownMenu>
-                        <DropdownMenuTrigger asChild>
-                          <Button variant="ghost" className="h-8 w-8 p-0">
-                            <span className="sr-only">Open menu</span>
-                            <MoreHorizontal />
-                          </Button>
-                        </DropdownMenuTrigger>
-                        <DropdownMenuContent align="end">
-                          <DropdownMenuLabel>Actions</DropdownMenuLabel>
-                          <DropdownMenuItem
-                            onClick={() => deleteTransaction(row)}
-                          >
-                            Delete
-                          </DropdownMenuItem>
-                        </DropdownMenuContent>
-                      </DropdownMenu>
-                    </TableCell>
-                  </TableRow>
-                );
-              })}
-            </TableBody>
-          </Table>
+                  return (
+                    <TableRow key={row.id}>
+                      <TableCell>
+                        <span>
+                          {formatDate({
+                            date: row.timestamp,
+                            options: md
+                              ? undefined
+                              : {
+                                  dateStyle: undefined,
+                                  day: "numeric",
+                                  month: "2-digit",
+                                  year: undefined,
+                                },
+                          })}
+                        </span>
+                      </TableCell>
+                      <TableCell>{row.description}</TableCell>
+                      <TableCell className="hidden md:table-cell">
+                        {row.category.name}
+                      </TableCell>
+                      <TableCell className="text-right">
+                        <Currency
+                          value={amount}
+                          options={{
+                            currency: row.currency,
+                            maximumFractionDigits: md ? 2 : 0,
+                          }}
+                          className={cn(
+                            isIncome && "text-green-600",
+                            isExpense && "text-red-600",
+                          )}
+                        />
+                      </TableCell>
+                      <TableCell>
+                        <DropdownMenu>
+                          <DropdownMenuTrigger asChild>
+                            <Button variant="ghost" className="h-8 w-8 p-0">
+                              <span className="sr-only">Open menu</span>
+                              <MoreHorizontal />
+                            </Button>
+                          </DropdownMenuTrigger>
+                          <DropdownMenuContent align="end">
+                            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+                            <DropdownMenuItem
+                              onClick={() => deleteTransaction(row)}
+                            >
+                              Delete
+                            </DropdownMenuItem>
+                          </DropdownMenuContent>
+                        </DropdownMenu>
+                      </TableCell>
+                    </TableRow>
+                  );
+                })}
+              </TableBody>
+            </Table>
+          </div>
         )}
       </div>
 
