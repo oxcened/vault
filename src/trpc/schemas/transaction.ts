852a576478b5a9a6fe43dcecb57ac9a52c70ab79
Alen Ajam <alen@fleek.xyz>
2025-03-08 08:54:10 +0100

    feat: add create transaction validation schema

---
+++ src/trpc/schemas/transaction.ts
@@ -0,0 +1,15 @@
+import { TransactionType } from "@prisma/client";
+import * as yup from "yup";
+
+export const createTransactionSchema = yup.object({
+  currency: yup.string().label("Currency").required(),
+  amount: yup.number().label("Amount").required(),
+  timestamp: yup.date().label("Timestamp").required(),
+  description: yup.string().label("Description").required(),
+  type: yup
+    .string()
+    .oneOf(Object.values(TransactionType))
+    .label("Description")
+    .required(),
+  categoryId: yup.string().label("Category ID").required(),
+});
