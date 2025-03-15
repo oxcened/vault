fce4733524371a52ff3c39967795d9a2a0332899
Alen Ajam <hello@alenajam.dev>
2025-03-15 12:56:44 +0100

    chore(chart): remove log

--- src/components/ui/chart.tsx
+++ src/components/ui/chart.tsx
@@ -189,7 +189,6 @@ const ChartTooltipContent = React.forwardRef<
             const key = `${nameKey || item.name || item.dataKey || "value"}`;
             const itemConfig = getPayloadConfigFromPayload(config, item, key);
             const indicatorColor = color || item.payload.fill || item.color;
-            console.log(item);
 
             return (
               <div
