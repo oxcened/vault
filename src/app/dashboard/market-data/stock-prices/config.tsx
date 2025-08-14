8b053c800528d86ca80435a006f2a7cb5a5e9fa7
Alen Ajam <hello@alenajam.dev>
2025-08-14 07:25:56 +0200

    chore(stock-prices): migrate table to data table

---
+++ src/app/dashboard/market-data/stock-prices/config.tsx
@@ -0,0 +1,113 @@
+import { createColumnHelper } from "@tanstack/react-table";
+import { RoundedCurrency } from "~/components/ui/number";
+import { api, RouterOutputs } from "~/trpc/react";
+import { formatDate } from "~/utils/date";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+} from "~/components/ui/dropdown-menu";
+import { Button } from "~/components/ui/button";
+import { MoreHorizontalIcon } from "lucide-react";
+import { useConfirmDelete } from "~/components/confirm-delete-modal";
+import { toast } from "sonner";
+import { useState } from "react";
+import { StockPriceHistory } from "@prisma/client";
+
+import EditStockPriceDialog from "./EditStockPriceDialog";
+
+const columnHelper =
+  createColumnHelper<RouterOutputs["stockPrice"]["getAll"][number]>();
+
+export const stockPricesColumns = [
+  columnHelper.accessor("timestamp", {
+    header: "Date",
+    cell: ({ getValue }) => {
+      return formatDate({ date: getValue() });
+    },
+  }),
+  columnHelper.accessor("ticker.ticker", {
+    header: "Ticker",
+  }),
+  columnHelper.accessor("ticker.exchange", {
+    header: "Exchange",
+  }),
+  columnHelper.accessor("price", {
+    header: "Price",
+    cell: ({ getValue }) => {
+      return <RoundedCurrency value={getValue()} />;
+    },
+    meta: {
+      cellClassName: "text-right",
+      headerClassName: "text-right",
+    },
+  }),
+  columnHelper.display({
+    id: "actions",
+    cell: ({ row }) => {
+      const utils = api.useUtils();
+
+      function handleStockChanged() {
+        void utils.stockPrice.getAll.invalidate();
+        void utils.netWorthOverview.get.invalidate();
+        void utils.netWorthAsset.getAll.invalidate();
+        void utils.netWorthAsset.getDetailById.invalidate();
+        void utils.dashboard.getSummary.invalidate();
+      }
+
+      const { mutate: deleteStockPrice } = api.stockPrice.delete.useMutation({
+        onSuccess: () => {
+          toast.success("Stock price deleted.");
+          void handleStockChanged();
+        },
+      });
+
+      const [editingPrice, setEditingPrice] = useState<StockPriceHistory>();
+      const [isEditDialogOpen, setEditDialogOpen] = useState(false);
+
+      const { confirm, modal } = useConfirmDelete();
+
+      function handleEditClick() {
+        setEditingPrice(row.original);
+        setEditDialogOpen(true);
+      }
+
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <span className="sr-only">Open menu</span>
+              <MoreHorizontalIcon />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+
+            <DropdownMenuItem onClick={handleEditClick}>Edit</DropdownMenuItem>
+            <DropdownMenuItem
+              onClick={() =>
+                confirm({
+                  itemType: "stock price",
+                  itemName: row.original.ticker.ticker,
+                  onConfirm: () => deleteStockPrice({ id: row.original.id }),
+                })
+              }
+            >
+              Delete
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+          {modal}
+          <EditStockPriceDialog
+            key={`edit-stock-price-dialog-${isEditDialogOpen}`}
+            isOpen={isEditDialogOpen}
+            stockPrice={editingPrice}
+            onOpenChange={setEditDialogOpen}
+            onSuccess={handleStockChanged}
+          />
+        </DropdownMenu>
+      );
+    },
+  }),
+];
--- src/app/dashboard/market-data/stock-prices/page.tsx
+++ src/app/dashboard/market-data/stock-prices/page.tsx
@@ -10,50 +10,27 @@ import {
 } from "~/components/ui/breadcrumb";
 import { Separator } from "~/components/ui/separator";
 import { SidebarTrigger } from "~/components/ui/sidebar";
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHeader,
-  TableHead,
-  TableRow,
-} from "~/components/ui/table";
 import { api } from "~/trpc/react";
 import { Button } from "~/components/ui/button";
-import { formatDate } from "~/utils/date";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "~/components/ui/dropdown-menu";
-import { MoreHorizontal, PencilIcon, Plus, Trash2Icon } from "lucide-react";
-import EditStockPriceDialog from "./EditStockPriceDialog";
-import { type StockPriceHistory } from "@prisma/client";
+import { Plus } from "lucide-react";
 import NewStockPriceDialog from "./NewStockPriceDialog";
 import { TableSkeleton } from "~/components/table-skeleton";
-import { toast } from "sonner";
-import { Number } from "~/components/ui/number";
-import { useConfirmDelete } from "~/components/confirm-delete-modal";
-import { Card } from "~/components/ui/card";
+import { useReactTable, getCoreRowModel } from "@tanstack/react-table";
+import { stockPricesColumns } from "./config";
+import { DataTable } from "~/components/ui/data-table";
 
 export default function StockPricesPage() {
   const { data = [], refetch, isPending } = api.stockPrice.getAll.useQuery();
 
-  const { mutate: deleteStockPrice } = api.stockPrice.delete.useMutation({
-    onSuccess: () => {
-      toast.success("Stock price deleted.");
-      void refetch();
-    },
+  const table = useReactTable({
+    data: data ?? [],
+    columns: stockPricesColumns,
+    getCoreRowModel: getCoreRowModel(),
   });
 
   const utils = api.useUtils();
 
-  const [editingPrice, setEditingPrice] = useState<StockPriceHistory>();
   const [isNewDialogOpen, setNewDialogOpen] = useState(false);
-  const [isEditDialogOpen, setEditDialogOpen] = useState(false);
 
   function handleStockCreatedOrEdited() {
     void refetch();
@@ -62,13 +39,6 @@ export default function StockPricesPage() {
     void utils.netWorthAsset.getDetailById.invalidate();
     void utils.dashboard.getSummary.invalidate();
   }
-
-  function handleEditClick(stockPrice: StockPriceHistory) {
-    setEditingPrice(stockPrice);
-    setEditDialogOpen(true);
-  }
-
-  const { confirm, modal } = useConfirmDelete();
 
   return (
     <>
@@ -98,74 +68,7 @@ export default function StockPricesPage() {
       </header>
 
       <div className="mx-auto w-full max-w-screen-md p-5">
-        {isPending && <TableSkeleton />}
-        {!isPending && !data.length && (
-          <div className="rounded-xl bg-muted p-10 text-center text-muted-foreground">
-            You don&apos;t have any stock prices yet
-          </div>
-        )}
-        {!isPending && !!data.length && (
-          <Card>
-            <Table>
-              <TableHeader>
-                <TableRow>
-                  <TableHead>Ticker</TableHead>
-                  <TableHead>Exchange</TableHead>
-                  <TableHead className="text-right">Price</TableHead>
-                  <TableHead>Timestamp</TableHead>
-                  <TableHead className="w-0"></TableHead>
-                </TableRow>
-              </TableHeader>
-              <TableBody>
-                {data.map((price) => (
-                  <TableRow key={price.id}>
-                    <TableCell>{price.ticker.ticker}</TableCell>
-                    <TableCell>{price.ticker.exchange}</TableCell>
-                    <TableCell className="text-right">
-                      <Number value={price.price} />
-                    </TableCell>
-                    <TableCell>
-                      {formatDate({ date: price.timestamp })}
-                    </TableCell>
-                    <TableCell>
-                      <DropdownMenu>
-                        <DropdownMenuTrigger asChild>
-                          <Button variant="ghost" className="h-8 w-8 p-0">
-                            <span className="sr-only">Open menu</span>
-                            <MoreHorizontal />
-                          </Button>
-                        </DropdownMenuTrigger>
-                        <DropdownMenuContent align="end">
-                          <DropdownMenuLabel>Actions</DropdownMenuLabel>
-                          <DropdownMenuSeparator />
-                          <DropdownMenuItem
-                            onClick={() => handleEditClick(price)}
-                          >
-                            <PencilIcon />
-                            Edit
-                          </DropdownMenuItem>
-                          <DropdownMenuItem
-                            onClick={() =>
-                              confirm({
-                                itemType: "stock price",
-                                itemName: price.ticker.ticker,
-                                onConfirm: () =>
-                                  deleteStockPrice({ id: price.id }),
-                              })
-                            }
-                          >
-                            <Trash2Icon />
-                            Delete
-                          </DropdownMenuItem>
-                        </DropdownMenuContent>
-                      </DropdownMenu>
-                    </TableCell>
-                  </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </Card>
-        )}
+        {isPending ? <TableSkeleton /> : <DataTable table={table} />}
       </div>
 
       <NewStockPriceDialog
@@ -174,16 +77,6 @@ export default function StockPricesPage() {
         onOpenChange={() => setNewDialogOpen(false)}
         onSuccess={handleStockCreatedOrEdited}
       />
-
-      <EditStockPriceDialog
-        key={`edit-stock-price-dialog-${isEditDialogOpen}`}
-        isOpen={isEditDialogOpen}
-        stockPrice={editingPrice}
-        onOpenChange={setEditDialogOpen}
-        onSuccess={handleStockCreatedOrEdited}
-      />
-
-      {modal}
     </>
   );
 }
