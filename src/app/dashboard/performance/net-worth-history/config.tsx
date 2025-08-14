2e71ef4884b9fa13f0f27ccf5e4a291c40f409be
Alen Ajam <hello@alenajam.dev>
2025-08-14 07:08:08 +0200

    chore(net-worth-history): migrate table to data table

---
+++ src/app/dashboard/performance/net-worth-history/config.tsx
@@ -0,0 +1,54 @@
+import { NetWorth } from "@prisma/client";
+import { createColumnHelper } from "@tanstack/react-table";
+import { RoundedCurrency } from "~/components/ui/number";
+import { cn } from "~/lib/utils";
+import { formatDate } from "~/utils/date";
+
+const columnHelper = createColumnHelper<NetWorth>();
+
+export const netWorthColumns = [
+  columnHelper.accessor("timestamp", {
+    header: "Date",
+    cell: ({ getValue }) => {
+      return formatDate({ date: getValue() });
+    },
+  }),
+  columnHelper.accessor("totalAssets", {
+    header: "Assets",
+    cell: ({ getValue }) => {
+      return <RoundedCurrency value={getValue()} />;
+    },
+    meta: {
+      cellClassName: "text-right",
+      headerClassName: "text-right",
+    },
+  }),
+  columnHelper.accessor("totalDebts", {
+    header: "Debts",
+    cell: ({ getValue }) => {
+      return <RoundedCurrency value={getValue()} />;
+    },
+    meta: {
+      cellClassName: "text-right",
+      headerClassName: "text-right",
+    },
+  }),
+  columnHelper.accessor("netValue", {
+    header: "Net worth",
+    cell: ({ getValue }) => {
+      return (
+        <RoundedCurrency
+          value={getValue()}
+          className={cn(
+            getValue().isPos() && "text-financial-positive",
+            getValue().isNeg() && "text-financial-negative",
+          )}
+        />
+      );
+    },
+    meta: {
+      cellClassName: "text-right",
+      headerClassName: "text-right",
+    },
+  }),
+];
--- src/app/dashboard/performance/net-worth-history/page.tsx
+++ src/app/dashboard/performance/net-worth-history/page.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { useReactTable, getCoreRowModel } from "@tanstack/react-table";
 import {
   Breadcrumb,
   BreadcrumbItem,
@@ -5,27 +8,21 @@ import {
   BreadcrumbPage,
   BreadcrumbSeparator,
 } from "~/components/ui/breadcrumb";
-import { Card } from "~/components/ui/card";
-import { RoundedCurrency } from "~/components/ui/number";
 import { Separator } from "~/components/ui/separator";
 import { SidebarTrigger } from "~/components/ui/sidebar";
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableFooter,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "~/components/ui/table";
-import { api } from "~/trpc/server";
-import { formatDate } from "~/utils/date";
+import { api } from "~/trpc/react";
+import { netWorthColumns } from "./config";
+import { TableSkeleton } from "~/components/table-skeleton";
+import { DataTable } from "~/components/ui/data-table";
 
-export default async function NwHistoryPage() {
-  const data = await api.netWorth.getAll();
-  const athAssets = Math.max(...data.map((item) => Number(item.totalAssets)));
-  const athDebts = Math.max(...data.map((item) => Number(item.totalDebts)));
-  const athNetValue = Math.max(...data.map((item) => Number(item.netValue)));
+export default function NwHistoryPage() {
+  const { data, isPending } = api.netWorth.getAll.useQuery();
+
+  const table = useReactTable({
+    data: data ?? [],
+    columns: netWorthColumns,
+    getCoreRowModel: getCoreRowModel(),
+  });
 
   return (
     <>
@@ -45,55 +42,7 @@ export default async function NwHistoryPage() {
         </Breadcrumb>
       </header>
       <div className="mx-auto w-full max-w-screen-md p-5">
-        {!data.length && (
-          <div className="rounded-xl bg-muted p-10 text-center text-muted-foreground">
-            You don&apos;t have a net worth history yet
-          </div>
-        )}
-        {!!data.length && (
-          <Card>
-            <Table>
-              <TableHeader>
-                <TableRow>
-                  <TableHead>Date</TableHead>
-                  <TableHead className="w-32 text-right">Assets</TableHead>
-                  <TableHead className="w-32 text-right">Debts</TableHead>
-                  <TableHead className="w-32 text-right">Net value</TableHead>
-                </TableRow>
-              </TableHeader>
-              <TableBody>
-                {data.map((row) => (
-                  <TableRow key={row.id}>
-                    <TableCell>{formatDate({ date: row.timestamp })}</TableCell>
-                    <TableCell className="text-right">
-                      <RoundedCurrency value={row.totalAssets} />
-                    </TableCell>
-                    <TableCell className="text-right">
-                      <RoundedCurrency value={row.totalDebts} />
-                    </TableCell>
-                    <TableCell className="text-right">
-                      <RoundedCurrency value={row.netValue} />
-                    </TableCell>
-                  </TableRow>
-                ))}
-              </TableBody>
-              <TableFooter>
-                <TableRow>
-                  <TableCell>All time high</TableCell>
-                  <TableCell className="text-right">
-                    <RoundedCurrency value={athAssets} />
-                  </TableCell>
-                  <TableCell className="text-right">
-                    <RoundedCurrency value={athDebts} />
-                  </TableCell>
-                  <TableCell className="text-right">
-                    <RoundedCurrency value={athNetValue} />
-                  </TableCell>
-                </TableRow>
-              </TableFooter>
-            </Table>
-          </Card>
-        )}
+        {isPending ? <TableSkeleton /> : <DataTable table={table} />}
       </div>
     </>
   );
