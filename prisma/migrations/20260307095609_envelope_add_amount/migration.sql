247e6b87592a3ec18852e4b098b11110e8d35901
Alen Ajam <hello@alenajam.dev>
2026-03-07 15:38:30 +0100

    fix: add default to migration

--- prisma/migrations/20260307095609_envelope_add_amount/migration.sql
+++ prisma/migrations/20260307095609_envelope_add_amount/migration.sql
@@ -1,8 +1,3 @@
-/*
-  Warnings:
-
-  - Added the required column `amount` to the `Envelope` table without a default value. This is not possible if the table is not empty.
-
-*/
 -- AlterTable
-ALTER TABLE "Envelope" ADD COLUMN     "amount" DECIMAL(38,18) NOT NULL;
+ALTER TABLE "Envelope"
+ADD COLUMN "amount" DECIMAL(38,18) NOT NULL DEFAULT 0;
