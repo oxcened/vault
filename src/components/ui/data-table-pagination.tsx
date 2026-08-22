3685a11804d6263bcee7f4975e6e8b748a200400
Alen Ajam <alen.ajam@live.com>
2026-08-22 22:26:42 +0200

    refactor: remove transaction selection summary

--- src/components/ui/data-table-pagination.tsx
+++ src/components/ui/data-table-pagination.tsx
@@ -23,12 +23,8 @@ export function DataTablePagination<TData>({
   table,
 }: DataTablePaginationProps<TData>) {
   return (
-    <div className="flex items-center justify-between px-1 md:px-2">
-      <div className="hidden flex-1 text-sm text-muted-foreground md:block">
-        {table.getFilteredSelectedRowModel().rows.length} of{" "}
-        {table.getFilteredRowModel().rows.length} row(s) selected.
-      </div>
-      <div className="flex w-full items-center justify-between gap-2 md:w-auto md:space-x-6 lg:space-x-8">
+    <div className="flex items-center px-1 md:px-2">
+      <div className="ml-auto flex w-full items-center justify-between gap-2 md:w-auto md:space-x-6 lg:space-x-8">
         <div className="hidden items-center space-x-2 sm:flex">
           <p className="text-sm font-medium">Rows per page</p>
           <Select
